d309fea9324f2ff2b998ecb12c6fbc035907e57a
Matteo <matteo.iorio01@gmail.com>
2022-11-24 23:58:40 +0100

    created the sam deployer

--- function.js
+++ function.js
@@ -1,4 +1,33 @@
+module.exports = lambdaFunction;
 const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("./app");
 
-exports.probotApp = createNodeMiddleware(app, { probot: createProbot() });
+const lowercaseKeys = require("lowercase-keys");
+
+async function lambdaFunction(probot, event) {
+  try {
+    // lowercase all headers to respect headers insensitivity (RFC 7230 $3.2 'Header Fields', see issue #62)
+    const headersLowerCase = lowercaseKeys(event.headers);
+
+    // this will be simpler once we ship `verifyAndParse()`
+    // see https://github.com/octokit/webhooks.js/issues/379
+    await probot.webhooks.verifyAndReceive({
+      id: headersLowerCase["x-github-delivery"],
+      name: headersLowerCase["x-github-event"],
+      signature:
+        headersLowerCase["x-hub-signature-256"] ||
+        headersLowerCase["x-hub-signature"],
+      payload: event.body,
+    });
+
+    return {
+      statusCode: 200,
+      body: '{"ok": "everything went well"}',
+    };
+  } catch (error) {
+    return {
+      statusCode: error.status || 500,
+      error: '{"error" : "Somethig went wrong. You should check the probot\'s informations"}',
+    };
+  }
+}
