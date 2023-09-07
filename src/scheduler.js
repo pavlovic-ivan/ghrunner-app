382a0f3d48163ecf910fa3659bd8a35bcb2af877
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-09-07 11:37:06 +0200

    Remove node-cron

--- src/scheduler.js
+++
@@ -1,8 +0,0 @@
-const nodeCron = require('node-cron');
-
-module.exports = function(){
-    nodeCron.schedule('* * * * * *', () => {
-        // This job will run every second
-        console.log(new Date().toLocaleTimeString());
-      })
-}
