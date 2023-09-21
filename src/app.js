efa216877d52dc7d27f7f70315d687dae4926bc3
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-09-21 15:44:51 +0200

    Remove unused env vars

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const { createOrDelete } = require("../infra")
 const uuid = require("uuid");
 const fs = require('fs');
-
-const jobFilter = process.env.JOB_FILTER;
+const { config } = require("process");
 
 /**
  * @param {import('probot').Probot} app
@@ -13,19 +12,16 @@ const probotApp = async (app) => {
     if(Array.isArray(context.payload.workflow_job.labels) && context.payload.workflow_job.labels.length > 0){
       var labels = context.payload.workflow_job.labels.join(',');
 
-      // if(jobFilter === labels){ old
       const rawConfigPerLabel = fs.readFileSync('config.json', 'utf8');
       const configPerLabel = JSON.parse(rawConfigPerLabel);
 
       if(configPerLabel.hasOwnProperty(labels)){
-        console.info(`CtxID=[${context.id}]. JobID=[${context.payload.workflow_job.id}]. Message=Received workflow job is a candidate for self hosted runners. JobUrl=[${context.payload.workflow_job.url}]`);
+        console.info(`CtxID=[${context.id}]. JobID=[${context.payload.workflow_job.id}]. Labels=[${labels}] Message=Received workflow job is a candidate for self hosted runners. JobUrl=[${context.payload.workflow_job.url}]`);
         console.log(`Received event from job: ${context.payload.workflow_job.id}. Action: ${context.payload.action}. Name: ${context.payload.workflow_job.name}`);
         var action = context.payload.action === 'completed' ? context.payload.action : (context.payload.action === 'queued' ? "requested": null);
         
         if(action !== null){
           console.log(`Job: ${context.payload.workflow_job.id}. Action: ${action}. Name: ${context.payload.workflow_job.name}. Run id: ${context.payload.workflow_job.run_id.toString()}. Run attempt: ${context.payload.workflow_job.run_attempt.toString()}. Labels: ${labels}`);
-  
-          console.log(configPerLabel[labels]);
 
           try {
             let stack_name; 
@@ -37,10 +33,10 @@ const probotApp = async (app) => {
 
             let repo_full_name = context.payload.repository.full_name.split('/');
             let config = {
-              machineType: process.env.MACHINE_TYPE,
-              machineImage: process.env.MACHINE_IMAGE,
-              bootDiskSizeInGB: process.env.BOOT_DISK_SIZE_IN_GB,
-              bootDiskType: process.env.BOOT_DISK_TYPE,
+              machineType: configPerLabel.machineType,
+              machineImage: configPerLabel.machineImage,
+              bootDiskSizeInGB: configPerLabel.bootDiskSizeInGB,
+              bootDiskType: configPerLabel.bootDiskType,
               owner: repo_full_name[0],
               repo: repo_full_name[1],
               labels: labels
