0785fd4f2efbeb8656b28a180fed407a09625ed7
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-10-06 13:41:40 +0200

    Add AWS SDK to list S3 objects

--- infra/index.js
+++ infra/index.js
@@ -157,12 +157,26 @@ async function handleStack(stack){
 }
 
 async function removeStateFiles(stackData){
-    console.log('Get backups using await');
-    const result = await s3.listObjectsV2({ 
+    console.log(`Deleting pulumi backups for [${stackData.ghrunnerName}]`);
+    const resultBackups = await s3.deleteObject({ 
         Bucket: process.env.PULUMI_BACKEND_URL.replace(/^s3:\/\//, ''),
-        Prefix: `.pulumi/backups/${stackData.repo}`
+        Key: `.pulumi/backups/${stackData.repo}/${stackData.ghrunnerName}`
     }).promise();
-    console.log(`Got result from using await: ${JSON.stringify(result)}`);
+    console.log(`Deleting pulumi backups result: ${JSON.stringify(resultBackups)}`);
+
+    console.log(`Deleting pulumi history for [${stackData.ghrunnerName}]`);
+    const resultHistory = await s3.deleteObject({ 
+        Bucket: process.env.PULUMI_BACKEND_URL.replace(/^s3:\/\//, ''),
+        Key: `.pulumi/history/${stackData.repo}/${stackData.ghrunnerName}`
+    }).promise();
+    console.log(`Deleting pulumi history result: ${JSON.stringify(resultHistory)}`);
+
+    console.log(`Deleting pulumi stacks for [${stackData.ghrunnerName}]`);
+    const resultStacks = await s3.deleteObject({ 
+        Bucket: process.env.PULUMI_BACKEND_URL.replace(/^s3:\/\//, ''),
+        Key: `.pulumi/stacks/${stackData.repo}/${stackData.ghrunnerName}`
+    }).promise();
+    console.log(`Deleting pulumi stacks result: ${JSON.stringify(resultStacks)}`);
 
     // console.log('Get backups');
     // s3.listObjectsV2({ 
