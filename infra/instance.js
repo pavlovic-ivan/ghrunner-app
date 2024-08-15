cec489d262dce641e4e824152971e036493cab48
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2024-08-16 01:01:28 +0200

    Add logic that searches for best az

--- infra/instance.js
+++ infra/instance.js
@@ -1,6 +1,25 @@
 const aws = require("@pulumi/aws");
 const pulumi = require("@pulumi/pulumi");
 const { getTags } = require("./commons");
+const { EC2Client, DescribeSpotPriceHistoryCommand } = require("@aws-sdk/client-ec2");
+
+async function findBestAvailabilityZone(instanceType, region) {
+    const client = new EC2Client({ region });
+
+    const spotPriceHistory = await client.send(new DescribeSpotPriceHistoryCommand({
+        InstanceTypes: [instanceType],
+        ProductDescriptions: ['Linux/UNIX'],
+        StartTime: new Date(Date.now() - 24 * 60 * 60 * 1000), // Last 24 hours
+    }));
+
+    return spotPriceHistory.SpotPriceHistory
+        .reduce((best, price) => {
+            const currentPrice = parseFloat(price.SpotPrice);
+            return (!best || currentPrice < best.price)
+                ? { az: price.AvailabilityZone, price: currentPrice }
+                : best;
+        }, null).az;
+}
 
 const createInstance = (identity, securityGroup, script, config) => {
     const ami = aws.ec2.getAmiOutput({
@@ -17,7 +36,7 @@ const createInstance = (identity, securityGroup, script, config) => {
         instanceType: config.machineType,
         tags: getTags(config.repo),
         vpcSecurityGroupIds: [ securityGroup.id ],
-        availabilityZone: pulumi.output(aws.getAvailabilityZones({})).names[0], // Choose the first available AZ
+        availabilityZone: pulumi.output(findBestAvailabilityZone(config.machineType, process.env.AWS_REGION)),
         userData: Buffer.from(script).toString("base64"),
         ebsBlockDevices: [{
             deviceName: "/dev/sda1",
