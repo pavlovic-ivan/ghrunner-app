e02236a0a00254cf3d83ede0dea08d34d6489a65
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2024-08-16 10:15:21 +0200

    remove spot instance config

--- infra/instance.js
+++ infra/instance.js
@@ -1,25 +1,5 @@
 const aws = require("@pulumi/aws");
-const pulumi = require("@pulumi/pulumi");
 const { getTags } = require("./commons");
-const { EC2Client, DescribeSpotPriceHistoryCommand } = require("@aws-sdk/client-ec2");
-
-async function findBestAvailabilityZone(instanceType, region) {
-    const client = new EC2Client({ region });
-
-    const spotPriceHistory = await client.send(new DescribeSpotPriceHistoryCommand({
-        InstanceTypes: [instanceType],
-        ProductDescriptions: ['Linux/UNIX'],
-        StartTime: new Date(Date.now() - 24 * 60 * 60 * 1000), // Last 24 hours
-    }));
-
-    return spotPriceHistory.SpotPriceHistory
-        .reduce((best, price) => {
-            const currentPrice = parseFloat(price.SpotPrice);
-            return (!best || currentPrice < best.price)
-                ? { az: price.AvailabilityZone, price: currentPrice }
-                : best;
-        }, null).az;
-}
 
 const createInstance = (identity, securityGroup, script, config) => {
     const ami = aws.ec2.getAmiOutput({
@@ -36,16 +16,12 @@ const createInstance = (identity, securityGroup, script, config) => {
         instanceType: config.machineType,
         tags: getTags(config.repo),
         vpcSecurityGroupIds: [ securityGroup.id ],
-        availabilityZone: pulumi.output(findBestAvailabilityZone(config.machineType, process.env.AWS_REGION)),
         userData: Buffer.from(script).toString("base64"),
         ebsBlockDevices: [{
             deviceName: "/dev/sda1",
             volumeSize: config.bootDiskSizeInGB,
             volumeType: config.bootDiskType
         }],
-        instanceMarketOptions: {
-            marketType: "spot"
-        }
     });
 }
 
